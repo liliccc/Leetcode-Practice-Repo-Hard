8a4f97db0398247b41feed5805c3dc74dcdcca88
liliccc <xianj.chen@qq.com>
2022-01-02 12:14:58 +0800

    1.2

---
+++ src/algorithm/level1PrefuxSum/SubarraysSumEqualsToKII.java
@@ -0,0 +1,31 @@
+package algorithm.level1PrefuxSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraysSumEqualsToKII {
+    /**
+     * @param nums: a list of integer
+     * @param k: an integer
+     * @return: return an integer, denote the minimum length of continuous subarrays whose sum equals to k
+     */
+    public int subarraySumEqualsKII(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        Map<Long, Integer> sumToIndex = new HashMap<>();
+        long prefixSum = 0;
+        sumToIndex.put(prefixSum, 0);
+        int minimumSize = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            if (sumToIndex.containsKey(prefixSum - k)) {
+                int index = sumToIndex.get(prefixSum - k);
+                int len = i + 1 - index;
+                minimumSize = Math.min(minimumSize, len);
+            }
+            sumToIndex.put(prefixSum, i + 1);
+        }
+        return (minimumSize == Integer.MAX_VALUE) ? -1 : minimumSize;
+    }
+}
