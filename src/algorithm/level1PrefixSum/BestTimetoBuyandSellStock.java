9dc6c472b59f3379fba2f9b94e047c0fff656204
liliccc <xianj.chen@qq.com>
2022-01-05 15:48:17 +0800

    1.4

---
+++ src/algorithm/level1PrefixSum/BestTimetoBuyandSellStock.java
@@ -0,0 +1,20 @@
+package algorithm.level1PrefixSum;
+
+public class BestTimetoBuyandSellStock {
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int profit = 0, minPrice = Integer.MAX_VALUE;
+        for (int price : prices) {
+            // save the prefix smallest price
+            minPrice = Math.min(price, minPrice);
+            profit = Math.max(price - minPrice, profit);
+        }
+        return profit;
+    }
+}
